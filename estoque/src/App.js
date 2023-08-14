d86f64bcbb86b9922749810def00d5b1fea71a9e
EduOC1022 <112728180+EduOC1022@users.noreply.github.com>
2023-08-14 13:18:56 -0300

    atualição

--- estoque/src/App.js
+++ estoque/src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 import Menu from "./Frontend/componentes/Menu";
 import Home from "./Frontend/paginas/Home";
 import Estoque from "./Frontend/paginas/Estoque";
+import axios from 'axios';
 import Cadastro from "./Frontend/paginas/Cadastro";
 import './App.css'
 
 
-
+axios.get('http://localhost:3000/db')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error('Erro na requisição:', error);
+  });
 
 function App() {
   return (
--- estoque/src/backend/bd.js
+++ estoque/src/backend/bd.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const {Pool} = require('pg');
 const db = new Pool ({
     user: 'postgres',
     password: 'stt123',
-    host: '192.168.3.33',
+    host: '150.162.186.12',
     port: 5432,
     database: 'estoque'
 });
+const porta = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/db', async (req, res) => {
+    try {
+      const pesquisa = 'SELECT * FROM cliente';
+      const resultados = await db.query(pesquisa);
+      const lista = resultados.rows;
+      
+      res.json(lista);
+    } catch (ex) {
+      console.log('Erro ao recuperar dados do banco de dados:', ex);
+      res.status(500).send('Erro ao recuperar dados do banco de dados.');
+    }
+  });
 
 db.connect();
 console.log("Conectado ao banco de dados");
 
-app.listen(3000, () => {
+app.listen(porta, () => {
     console.log('Servidor iniciado na porta 3000.');
 });
