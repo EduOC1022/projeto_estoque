ead5ddfd77dc3695bb2b07f36102293a2eb74b1d
ana.schwarz <anaflasch@gmail.com>
2023-08-15 13:27:25 -0300

    alteracao versao react

--- estoque/src/Frontend/paginas/CadastroC.js
+++ estoque/src/Frontend/paginas/CadastroC.js
@@ -3,6 +3,7 @@ import LibraryAddCheckIcon from '@mui/icons-material/LibraryAddCheck';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import Headin from "../componentes/Typographies/Headin";
 import AddIcon from '@mui/icons-material/Add';
+import {Container,Button, Grid, TextField } from "@mui/material";
 
 
 export default function CadastroC() {
@@ -15,10 +16,34 @@ export default function CadastroC() {
                 {
                     nome: 'Selecionar',
                     icone: <LibraryAddCheckIcon fontSize="large"/>
-                }
-                ]
+                }];
+    
+     const handleSubmit = () => { 
+
+     }
+
 
     return (
-        <Headin icones={icones} pagina='Cadastro Cliente'/>
+        <>
+            <Headin icones={icones} pagina='Cadastro Cliente'/>
+            <Container sx={{padding: 8, backgroundColor: 'primary.dark'}}>
+                <Grid container spacing={2}>
+                    <Grid item  md={2}>
+                        <TextField sx={{flexGrow: 1}} label= 'Nome'/>
+                    </Grid>
+                    <Grid item md={4}>
+                        <TextField sx={{flexGrow: 1}} label= 'Sobrenome'/>
+                    </Grid>
+                    <Grid item  md={3}>
+                        <TextField label= 'CPF'/>
+                    </Grid>
+                    <Grid item md={3}>
+                        <TextField label= 'Telefone'/>
+                    </Grid>
+                </Grid>
+                <Button/>
+            </Container>
+        </>
+
     );
 }
