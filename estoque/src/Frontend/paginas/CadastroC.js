05285e56e07755276566c2de310db411eb7410f3
EduOC1022 <112728180+EduOC1022@users.noreply.github.com>
2023-08-17 14:27:55 -0300

    teste

--- estoque/src/Frontend/paginas/CadastroC.js
+++ estoque/src/Frontend/paginas/CadastroC.js
@@ -4,8 +4,22 @@ import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import Headin from "../componentes/Typographies/Headin";
 import AddIcon from '@mui/icons-material/Add';
 import axios from'axios';
-import {Container, Box, Button, Grid, TextField } from "@mui/material";
+import {Container, Box, Button, Grid, TextField, 
+        Table, TableBody, TableCell, TableContainer,   
+        TableHead, TableRow, Paper} 
+        from "@mui/material";
 
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+    }
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    ];
 
 export default function CadastroC() {
     const icones =[{
@@ -44,9 +58,8 @@ export default function CadastroC() {
     return (
         <>
             <Headin icones={icones} pagina='Cadastro Cliente'/>
-            
                 <Grid container spacing={2} >
-                    <Grid item  xs={12} md={6}>
+                    <Grid item  xs={12} md={4}>
                     <Box sx={{backgroundColor: 'primary.dark', display: 'flex', justifyContent: 'center'}}>
                         <Container sx={{padding: 8, backgroundColor: 'secondary.light', margin: 8}}>
                             <form onSubmit={handleSubmit}>
@@ -71,12 +84,39 @@ export default function CadastroC() {
                     </Grid>
                     <Grid item  xs={12} md={6}>
                         <Box>
-
+                        <TableContainer component={Paper}>
+                            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                <TableHead>
+                                <TableRow>
+                                    <TableCell>Dessert (100g serving)</TableCell>
+                                    <TableCell align="right">Calories</TableCell>
+                                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                                </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow
+                                    key={row.name}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.calories}</TableCell>
+                                    <TableCell align="right">{row.fat}</TableCell>
+                                    <TableCell align="right">{row.carbs}</TableCell>
+                                    <TableCell align="right">{row.protein}</TableCell>
+                                    </TableRow>
+                                ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
                         </Box>
                     </Grid>
                 </Grid>
-           
-            
+                
         </>
 
     );
